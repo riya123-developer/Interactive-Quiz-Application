e6246dad2abfed3585895f97c8fa173baa9ed711
riya123-developer <riyasontakke03@gmail.com>
2025-04-05 13:42:14 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,61 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stand for?",
+        answer: "C. Hyper Text Markup Language",
+        options: [
+            "A. Hyper Type Multi Language",
+            "B. Hyper Text Multiple Language",
+            "C. Hyper Text Markup Language",
+            "D. Home Text Multi Language",
+        ]
+
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "A. Cascading Style Sheet",
+        options: [
+            "A. Cascading Style Sheet",
+            "B. Cute Style Sheet",
+            "C. Computer Style Sheet",
+            "D. Component Style Sheet",
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "D. Hypertext Preprocessor",
+        options: [
+            "A. Programming Hypertext Preprocessor",
+            "B. Hometext Programming ",
+            "C. Hypertext programming",
+            "D. Hypertext Preprocessor",
+    ]
+    },
+    {
+        numb: 4,
+        question: "What Does SQL Stand for?",
+        answer: "D. Structured Query Language",
+        options: [
+            "A. Strength Query Language",
+            "B. Stylesheet Query Language",
+            "C. Science Question Language",
+            "D. Structured Query Language",
+
+        ]
+    },
+    {
+        numb: 5,
+        question: "What Does XML Stand for",
+        answer: "B. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Extensible Markup Language",
+            "C. Extra Markup Language",
+            "D. Explore Multiple Language",
+
+        ]
+
+    },
+];
